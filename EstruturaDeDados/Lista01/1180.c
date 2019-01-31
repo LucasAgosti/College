9f28046e0a67139c713a9578fd85ecb6f2ee165d
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2019-01-31 11:10:02 -0300

    URI

---
+++ EstruturaDeDados/Lista01/1180.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+	int N, arr[1000], menorValor, posMenor = 0;
+
+	scanf("%d", &N);
+	
+	for(int i = 0; i < N; i++){
+		scanf("%d", &arr[i]);
+	
+		if(arr[i] < menorValor){
+			menorValor = arr[i];
+			posMenor = i;
+		}
+	}
+	
+	printf("Menor valor: %d\n", menorValor);
+	printf("Posicao: %d\n", posMenor);
+	
+	return 0;
+}
