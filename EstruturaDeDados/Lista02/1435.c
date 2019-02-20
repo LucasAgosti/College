b50dd83af29675ea580a203cabb8903ff9e0a1ee
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2019-02-20 15:32:36 -0300

    URI

---
+++ EstruturaDeDados/Lista02/1435.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define LINHAS 100
+#define COLUNAS 100
+
+void montaMatriz(int matriz[][COLUNAS], int N);
+
+int main (){
+    
+    int i, j, N, matriz[LINHAS][COLUNAS];
+
+    while(scanf("%d", &N), N != 0) {
+        
+        montaMatriz(matriz, N);
+    
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++) {
+                if (matriz[i][j] >= 100) {
+                    printf("%d", matriz[i][j]);
+                }else {
+                    printf("% 3d", matriz[i][j]);
+                }
+        
+                if (j < N - 1) {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
+
+void montaMatriz(int matriz[][COLUNAS], int N) {
+    
+	int flag, ij, i, j, aux;
+
+	for (flag = 0; flag < N; flag++) {
+    		
+    		ij = flag;
+    		i = ij;
+		aux = flag + 1;
+       
+
+		for (j = ij; j < N - ij; j++) {
+			matriz[i][j] = aux;
+			matriz[j][i] = aux;
+		}
+		
+		i = N - ij - 1;
+	
+		for (j = ij; j < N - ij; j++) {
+			matriz[i][j] = aux;
+			matriz[j][i] = aux;
+		}
+	}
+}
