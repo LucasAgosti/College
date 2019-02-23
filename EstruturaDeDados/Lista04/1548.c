28a1395d8f1730894316521a4a8c89316778fa7e
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2019-02-23 15:51:08 -0300

    URI

---
+++ EstruturaDeDados/Lista04/1548.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+#define SIZE 1000
+
+void imprimeArray(int arr[SIZE], int filaSize);
+void selectionSort(int arr[SIZE], int nAlunos);
+
+
+int main(){
+
+
+	int N, nAlunos, i, flag;
+	int notas[SIZE], posicaoInicial[SIZE];
+
+	scanf("%d", &N);
+	while(N-- > 0){
+
+		scanf("%d", &nAlunos);
+
+		for(i = 0; i < nAlunos; i++){
+			scanf("%d", &notas[i]);
+			posicaoInicial[i] = notas[i];
+		}
+
+		imprimeArray(notas, nAlunos);
+		selectionSort(notas, nAlunos);
+
+		flag = 0;
+
+		for(i = 0; i < nAlunos; i++){
+
+			if(notas[i] != posicaoInicial[i])
+				flag++;	
+		}
+		imprimeArray(notas, nAlunos);
+		
+		printf("%d\n", flag);
+	}
+
+
+	return 0;
+}
+
+void imprimeArray(int arr[SIZE], int filaSize){
+
+	for(int i = 0; i < filaSize; i++)
+		printf("|%d| ", arr[i]);
+	printf("\n");
+}
+
+void selectionSort(int arr[SIZE], int nAlunos){
+
+	int posicaoMenorValor, aux, i, j;
+
+	for(i = 0; i < nAlunos; i++){
+		
+		posicaoMenorValor = i;
+		
+		for(j = i + 1; j < nAlunos; j++){
+		
+			if(arr[j] < arr[posicaoMenorValor]){
+				posicaoMenorValor = j;
+			}
+			
+			if(posicaoMenorValor != i){
+				aux = arr[i];
+				arr[i] = arr[posicaoMenorValor];
+				arr[posicaoMenorValor] = aux;
+			
+			}
+		}
+	}
+}
